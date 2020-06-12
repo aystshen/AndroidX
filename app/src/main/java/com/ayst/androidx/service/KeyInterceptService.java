0cfe67c22a5903e45afd886f516a7162e0fff344
shenhaibo <ayst.shen@foxmail.com>
2020-06-12 10:07:58 +0800

    键值拦截api增加日志

--- app/src/main/java/com/ayst/androidx/service/KeyInterceptService.java
+++ app/src/main/java/com/ayst/androidx/service/KeyInterceptService.java
@@ -4,12 +4,14 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
 
 import com.ayst.androidx.IKeyInterceptService;
 import com.ayst.androidx.util.AppUtils;
 import com.ayst.androidx.util.SPUtils;
 
 public class KeyInterceptService extends Service {
+    private final static String TAG = "KeyInterceptService";
 
     public KeyInterceptService() {
     }
@@ -23,6 +25,8 @@ public class KeyInterceptService extends Service {
 
         @Override
         public void openKeyIntercept() throws RemoteException {
+            Log.i(TAG, "openKeyIntercept");
+
             AppUtils.openAccessibilityService(getApplicationContext());
             SPUtils.get(KeyInterceptService.this)
                     .saveData(SPUtils.KEY_KEY_INTERCEPT, true);
@@ -30,6 +34,8 @@ public class KeyInterceptService extends Service {
 
         @Override
         public void closeKeyIntercept() throws RemoteException {
+            Log.i(TAG, "closeKeyIntercept");
+
             AppUtils.closeAccessibilityService(getApplicationContext());
             SPUtils.get(KeyInterceptService.this)
                     .saveData(SPUtils.KEY_KEY_INTERCEPT, false);
